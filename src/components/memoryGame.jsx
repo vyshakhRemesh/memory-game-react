a646a1721856f3e0c8344f8c1db92edd192f03f4
vyshakh <vyshakh@dexlock.com>
2024-12-18 11:42:39 +0530

    created the react app using vite and setup tailwincss config, also created the memorGame Component in which the ui for selecting the number of grids was setup and also setup the function initializeGame in which i wrote the logic for the card pairs and shuffling them and setcards

---
+++ src/components/memoryGame.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+
+const MemoryGame = () => {
+  const [gridSize, setGridSize] = useState(4);
+  const [cards, setCards] = useState([]);
+
+  const [flipped, setFlipped] = useState([]);
+  const [solved, setSolved] = useState([]);
+
+  const [disabled, setDisabled] = useState(false);
+  const [won, setWon] = useState(false);
+
+  const handleGridSizeChange = (e) => {
+    const size = parseInt(e.target.value);
+    if (size >= 2 && size <= 10) {
+      setGridSize(size);
+    }
+  };
+
+  const initializeGame = () => {
+    const totalCards = gridSize * gridSize;
+    const pairCount = Math.floor(totalCards / 2);
+    const numbers = [...Array(pairCount).keys()].map((n) => n + 1);
+    const shuffledCards = [...numbers, ...numbers]
+      .sort(() => Math.random() - 0.5)
+      // .slice(0, totalCards);
+      .map((number, index) => ({ id: index, number }));
+    console.log(shuffledCards);
+
+    setCards(shuffledCards);
+    setFlipped([]);
+    setSolved([]);
+    setWon(false);
+  };
+
+  useEffect(() => {
+    initializeGame();
+  }, [gridSize]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold mb-6">Memory Game</h1>
+      {/* Input */}
+      <div className="mb-4">
+        <label className="mr-2" htmlFor="gridSize">
+          Grid Size:(max10)
+        </label>
+        <input
+          type="number"
+          id="gridSize"
+          min="2"
+          max="10"
+          value={gridSize}
+          onChange={handleGridSizeChange}
+          className="border-2 border-gray-300 rounded px-2 py-1"
+        />
+      </div>
+      {/* Game Board */}
+
+      {/* Result */}
+
+      {/* Reset / Play Again Btn */}
+    </div>
+  );
+};
+export default MemoryGame;
